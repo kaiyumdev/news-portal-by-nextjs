5bf5bb412bf68f818341fffaa9a2a15a655426a4
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-10-30 15:14:36 +0600

    news-portal-by-nextjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
 import Banner from "@/components/UI/Banner";
+import AllNews from "@/components/UI/AllNews";
 
-
-const HomePage = () => {
+const HomePage = ({ allNews }) => {
+  console.log(allNews);
   return (
     <>
       <Head>
@@ -16,6 +17,7 @@ const HomePage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Banner />
+      <AllNews allNews={allNews}></AllNews>
     </>
   );
 };
@@ -23,4 +25,17 @@ export default HomePage;
 
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/news");
+  const data = await res.json();
+  console.log(data);
+
+  return {
+    props: {
+      allNews: data,
+    },
+    revalidate: 10,
+  };
 };
